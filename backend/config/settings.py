b1b75b1a2a06c9ca94002f049e00d181938e15bb
alirezasepehri <elibenedictskyler@gmail.com>
2025-12-27 15:59:19 +0330

    fix(settings): replace invalid app name with 'rest_framework_simplejwt'

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', 'corsheaders',
-    'restـframeworkـsimplejwt',
+    'rest_framework_simplejwt',
     'users', 'lessons', 'cart', 'orders',
 ]
 
