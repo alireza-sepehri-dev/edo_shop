9a75f5d0ff673455cc721e49259cd0fd8cca07cf
alirezasepehri <elibenedictskyler@gmail.com>
2025-12-27 15:48:47 +0330

    fix(settings): correct app name from 'rest-framework-simplejwt' to 'rest_framework_simplejwt'

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', 'corsheaders',
-    'rest-framework-simplejwt',
+    'restـframeworkـsimplejwt',
     'users', 'lessons', 'cart', 'orders',
 ]
 
